5ea7e069ff9ae6e61da2cccf772e18020a7e6a19
Nadezda Kashirina <refenement@mail.ru>
2019-07-11 17:03:16 +0700

    Initial commit

---
+++ init.el
@@ -0,0 +1,66 @@
+;; init.el --- Emacs configuration
+
+;; INSTALL PACKAGES
+;; --------------------------------------
+
+(require 'package)
+
+(add-to-list 'package-archives
+       '("melpa" . "http://melpa.org/packages/") t)
+
+(package-initialize)
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar myPackages
+  '(material-theme
+    ))
+
+(mapc #'(lambda (package)
+    (unless (package-installed-p package)
+      (package-install package)))
+      myPackages)
+
+;; BASIC CUSTOMIZATION
+;; --------------------------------------
+
+(setq inhibit-startup-message t) ;; hide the startup message
+(load-theme 'material t) ;; load material theme
+(global-linum-mode t) ;; enable line numbers globally
+
+(tooltip-mode      -1)
+;(menu-bar-mode     -1) ;; отключаем графическое меню
+(tool-bar-mode     -1) ;; отключаем tool-bar
+;(scroll-bar-mode   -1) ;; отключаем полосу прокрутки
+(blink-cursor-mode -1) ;; курсор не мигает
+(setq use-dialog-box     nil) ;; никаких графических диалогов и окон - все через минибуфер
+;(setq redisplay-dont-pause t)  ;; лучшая отрисовка буфера
+(setq ring-bell-function 'ignore) ;; отключить звуковой сигнал
+
+;; PYTHON CONFIGURATION
+;; --------------------------------------
+
+(package-initialize)
+; Enable elpy mode
+(elpy-enable)
+; Fixing a key binding bug in elpy
+;;(define-key yas-minor-mode-map (kbd "C-c k") 'yas-expand)
+; Fixing another key binding bug in iedit mode
+(define-key global-map (kbd "C-c o") 'iedit-mode)
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (iedit yasnippet-snippets elpy material-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+;; init.el ends here
